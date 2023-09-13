b9bd968824a7e28d91a9337b020f0eb48db9c8b5
rslepfso <rslepfso@gmail.com>
2023-09-13 15:15:56 +0300

    finish part2a

--- index.js
+++ index.js
@@ -70,6 +70,21 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:", request.path);
+  console.log("Body:", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(requestLogger);
+app.use(unknownEndpoint);
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
