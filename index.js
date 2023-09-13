54fd2856249a3043130c19dc271b60504032d518
rslepfso <rslepfso@gmail.com>
2023-09-13 16:16:40 +0300

    finish part 3b

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
 
 let notes = [
   {
@@ -85,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 app.use(requestLogger);
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
