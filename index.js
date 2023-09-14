86844d381eda41002494d224d4ae43c432e7c772
rslepfso <rslepfso@gmail.com>
2023-09-14 12:50:56 +0300

    finish part3c2

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const Note = require("./models/note");
 
 const app = express();
 
@@ -7,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
 
+// STATIC NOTES BACKEND
 let notes = [
   {
     id: 1,
@@ -25,23 +29,21 @@ let notes = [
   },
 ];
 
+// ROUTE HANDLERS
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+// Get all notes
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((result) => {
+    response.json(result);
+  });
 });
 
+// Get single note
 app.get("/api/notes/:id", (request, response) => {
-  const id = +request.params.id;
-  console.log(id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(request.params.id).then((result) => response.json(result));
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -56,6 +58,7 @@ const generateId = () => {
   return maxId + 1;
 };
 
+// Create new note
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
@@ -63,17 +66,15 @@ app.post("/api/notes", (request, response) => {
     return response.status(400).json({ error: "content is missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes.concat(note);
-
-  response.json(note);
+  note.save().then((result) => response.json(result));
 });
 
+// MIDDLEWARE
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:", request.path);
@@ -89,7 +90,8 @@ const unknownEndpoint = (request, response) => {
 app.use(requestLogger);
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+// PORT AND APP
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ models/note.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+console.log("Connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("Connected");
+  })
+  .catch((error) => {
+    console.log("Connection error", error.message);
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
