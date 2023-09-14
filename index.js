5007355955a26ab052816eed23d7085eb4492208
rslepfso <rslepfso@gmail.com>
2023-09-14 14:09:05 +0300

    finish part 3c

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const Note = require("./models/note");
 
 const app = express();
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
 
 // STATIC NOTES BACKEND
 let notes = [
@@ -42,15 +42,29 @@ app.get("/api/notes", (request, response) => {
 });
 
 // Get single note
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((result) => response.json(result));
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((result) => {
+      if (result) {
+        response.json(result);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+      // console.log(error.message);
+      // response.status(400).send({ error: "Malformatted ID" });
+    });
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = +request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+// Delete note
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const generateId = () => {
@@ -74,7 +88,31 @@ app.post("/api/notes", (request, response) => {
   note.save().then((result) => response.json(result));
 });
 
+// Update note
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((result) => response.json(result))
+    .catch((error) => next(error));
+});
+
 // MIDDLEWARE
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted ID" });
+  }
+
+  next(error);
+};
+
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:", request.path);
@@ -89,6 +127,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(requestLogger);
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 // PORT AND APP
 const PORT = process.env.PORT;
